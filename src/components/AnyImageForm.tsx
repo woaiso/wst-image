4061d62617a451a06873ec58d901b7e441997831
廖云富 <yunful@jumei.com>
2019-06-02 22:18:41 +0800

    增加react-color

--- src/components/AnyImageForm.tsx
+++ src/components/AnyImageForm.tsx
@@ -1,31 +1,38 @@
 import React from 'react';
-import {
-  Form,
-  Row,
-  Col,
-  Input,
-  Select,
-  InputNumber,
-  Switch,
-  Radio,
-  Rate,
-  Checkbox,
-  Upload,
-  Button,
-  Icon,
-  Slider,
-} from 'antd';
+import { Form, Row, Col, Input, Select, InputNumber, Button, Icon, Popover } from 'antd';
+import { SketchPicker } from 'react-color';
 
-import styles from './form.css';
+import styles from './form.less';
 
 const { Option } = Select;
 class AnyImageForm extends React.Component<any, any> {
+  state = {
+    bgColor: '#1890FF',
+    displayBgColorPicker: false,
+  };
   handleSubmit = () => {};
   normFile = (e: { fileList: any }) => {
     if (Array.isArray(e)) {
       return e;
     }
     return e && e.fileList;
+  };
+  handleColorChange = (color: any) => {
+    this.setState({
+      bgColor: color.hex.toUpperCase(),
+    });
+    return false;
+  };
+  handleOpenColorPicker = () => {
+    this.setState({
+      displayBgColorPicker: true,
+    });
+    return false;
+  };
+  handleClose = () => {
+    this.setState({
+      displayBgColorPicker: false,
+    });
   };
   render() {
     const { getFieldDecorator } = this.props.form;
@@ -47,41 +54,55 @@ class AnyImageForm extends React.Component<any, any> {
           </Form.Item>
           <Form.Item label="高度" help="支持范围：10 ~ 2048">
             {getFieldDecorator('height', {
-              initialValue:480,
+              initialValue: 480,
               rules: [{ required: true, message: '请输入正确的图片高度!' }],
             })(<InputNumber min={10} max={2048} style={{ width: '100%' }} />)}
           </Form.Item>
 
           <Form.Item label="圆角" help="支持范围：0 ~ 100">
             {getFieldDecorator('radius', {
-              initialValue:0,
+              initialValue: 0,
               rules: [{ required: false, message: '请输入正确的圆角大小!' }],
             })(<InputNumber min={0} max={100} style={{ width: '100%' }} />)}
           </Form.Item>
 
           <Form.Item label="内边距" help="参考格式：10 20 10 20">
             {getFieldDecorator('padding', {
-              initialValue:0,
+              initialValue: 0,
               rules: [{ required: false, message: '请输入正确的圆角大小!' }],
             })(<Input style={{ width: '100%' }} />)}
           </Form.Item>
 
           <Form.Item label="背景颜色">
             {getFieldDecorator('bg_color', {
-              initialValue:'#1890FF',
+              initialValue: this.state.bgColor,
               rules: [],
-            })(<Input />)}
+            })(
+              <Input.Search
+                enterButton={<div style={{ background: this.state.bgColor }} />}
+                className={styles['color-picker-button']}
+                onSearch={this.handleOpenColorPicker}
+              />,
+            )}
+            <div className={styles['color-picker']}>
+              {this.state.displayBgColorPicker ? (
+                <div className={styles['color-picker']}>
+                  <div className={styles.cover} onClick={this.handleClose} />
+                  <SketchPicker color={this.state.bgColor} onChange={this.handleColorChange} />
+                </div>
+              ) : null}
+            </div>
           </Form.Item>
 
           <Form.Item label="文本颜色">
             {getFieldDecorator('color', {
-              initialValue:'#FFFFFF',
+              initialValue: '#FFFFFF',
               rules: [],
             })(<Input />)}
           </Form.Item>
           <Form.Item label="图片格式" hasFeedback={true}>
             {getFieldDecorator('format', {
-              initialValue:'png',
+              initialValue: 'png',
               rules: [{ required: false, message: '请选择一个导出的图片格式' }],
             })(
               <Select placeholder="请选择一个图片格式">
