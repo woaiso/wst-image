f2ac99aee476052de5939621548771f9c51a0ec9
廖云富 <yunful@jumei.com>
2019-06-03 20:10:23 +0800

    增加代理到python

--- .umirc.ts
+++ .umirc.ts
@@ -23,6 +23,14 @@ const config: IConfig =  {
       },
     }],
   ],
+  proxy: {
+    "/api": {
+      "target": "http://127.0.0.1:5000",
+      "changeOrigin": true,
+      "pathRewrite": { "^/api" : "" }
+    }
+  }
+
 }
 
 export default config;
